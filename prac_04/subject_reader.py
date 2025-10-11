fb52bdce87abd67280c137375c1bb47dc9723191
jd115459 <jackson.scrivener@my.jcu.edu.au>
2025-10-11 15:45:01 +1000

    commiting subject files for converting data strings to lists

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,7 +1,29 @@
-def print_report(incomes, number_of_months):
-    print("\nIncome Report\n-------------")
-    total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
-        total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+"""
+CP1404/CP5632 Practical
+Data file -> lists program
+"""
+
+FILENAME = "subject_data.txt"
+
+
+def main():
+    data = load_data(FILENAME)
+    print(data)
+
+
+def load_data(filename=FILENAME):
+    """Read data from file formatted like: subject,lecturer,number of students."""
+    input_file = open(filename)
+    for line in input_file:
+        print(line)  # See what a line looks like
+        print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        parts = line.split(',')  # Separate the data into its parts
+        print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        print(parts)  # See if that worked
+        print("----------")
+    input_file.close()
+
+
+main()
